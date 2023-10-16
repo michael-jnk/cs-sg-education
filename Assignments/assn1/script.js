70a355ee87b9a1fc5c4a79c32ebe80df9feae319
Michael <140859932+michael-jnk@users.noreply.github.com>
2023-10-16 15:46:06 -0400

    A1 added color and effects to projects

--- Assignments/assn1/script.js
+++ Assignments/assn1/script.js
@@ -1,4 +1,7 @@
 // just adding project clicking & changing description
+// ...and changing some colors too
+
+let style = getComputedStyle(document.body)
 
 function projectDisplay(projectName) {
     // console.log("project-"+projectName)
@@ -8,5 +11,10 @@ function projectDisplay(projectName) {
         // console.log(element.className)
         element.style.display = (element.className.includes("project-"+projectName)) ? "block" : "none";
         // console.log(element.className.includes("project-"+projectName))
+    }
+
+    for (element of document.getElementById("projects-flex").children) {
+        element.style.backgroundColor = (element.children.item(0).id === ("project-"+projectName))
+        ? style.getPropertyValue("--big-text-color") : style.getPropertyValue("--bg-color");
     }
 }
