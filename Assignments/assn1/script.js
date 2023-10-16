43d2841e000f1cd42ecc8803d68b458cb974453e
Michael <140859932+michael-jnk@users.noreply.github.com>
2023-10-16 15:02:33 -0400

    A1 added projects functionality

--- Assignments/assn1/script.js
+++ Assignments/assn1/script.js
@@ -1,1 +1,12 @@
 // just adding project clicking & changing description
+
+function projectDisplay(projectName) {
+    // console.log("project-"+projectName)
+    // console.log(document.getElementById("project-example"))
+    for (element of document.getElementById("project-example").children) {
+        // console.log(element)
+        // console.log(element.className)
+        element.style.display = (element.className.includes("project-"+projectName)) ? "block" : "none";
+        // console.log(element.className.includes("project-"+projectName))
+    }
+}
